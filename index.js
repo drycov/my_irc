ddf8fa17b8dceeea9e7c115a1fe47f827cf25c4b
Oritorius(Denis Rykov) <drycov@gmail.com>
2023-02-22 03:19:42 +0600

    dad

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const IRC = require('irc-server');
 const server = IRC.createServer();
-server.
-server.listen(6667);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+// server.listen(6667);
